736e36e332d0174215e28430a27690ad624a6d9c
win10 <mailvvc@gmail.com>
2025-04-16 17:57:02 +0200

    First commit

---
+++ src/main/java/phonebook/core/ApplicationManager.java
@@ -0,0 +1,73 @@
+package phonebook.core;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import phonebook.fw.ContactHelper;
+import phonebook.fw.HomePageHelper;
+import phonebook.fw.UserHelper;
+
+import java.time.Duration;
+
+public class ApplicationManager {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    private final String browser;
+    ContactHelper contactHelper;
+    HomePageHelper homePageHelper;
+    UserHelper userHelper;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+    protected void init() {
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("chrome_headless")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("headless");
+            options.addArguments("window-size=1920x1080");
+            driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        } else if (browser.equalsIgnoreCase("safari")) {
+            driver = new SafariDriver();
+        }  else {
+            //driver = new ChromeDriver();
+            throw new IllegalArgumentException("❌ Некорректный браузер: " + browser + ". Доступные варианты: chrome, firefox, edge, safari.");
+        }
+        //driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(2)); // Явное ожидание
+        driver.get("https://telranedu.web.app/home");
+        driver.manage().window().setPosition(new Point(2500, 0));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2)); // Неявное ожидание
+
+        contactHelper = new ContactHelper(driver, wait);
+        homePageHelper = new HomePageHelper(driver, wait);
+        userHelper = new UserHelper(driver, wait);
+    }
+
+    public void stop() {
+        driver.quit();
+    }
+
+    public ContactHelper getContactHelper() {
+        return contactHelper;
+    }
+
+    public HomePageHelper getHomePageHelper() {
+        return homePageHelper;
+    }
+
+    public UserHelper getUserHelper() {
+        return userHelper;
+    }
+}
---
+++ src/test/java/phonebook/tests/DeleteContactTests.java
@@ -0,0 +1,57 @@
+package phonebook.tests;
+
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import phonebook.core.TestBase;
+
+public class DeleteContactTests extends TestBase {
+    @BeforeMethod
+    public void precondition() {
+        app.getUserHelper().login("portishead@gmail.com", "Password@1");
+        app.getUserHelper().checkSuccessLogin();
+        //app.getContactHelper().addNewContact(app.getContactHelper().CONTACT_NAME);
+        //app.getContactHelper().addNewContact(app.getContactHelper().CONTACT_NAME);
+        //app.getContactHelper().addNewContact(app.getContactHelper().CONTACT_NAME);
+    }
+    Logger logger = LoggerFactory.getLogger(DeleteContactTests.class);
+    @Test
+    public void creatOneAndDeleteOnePositiveTest() {
+        // Количество контактов ДО удаления.
+        int sizeBefore = app.getContactHelper().getContactsCount();
+        //System.out.println("Кол-во контактов ДО удаления: " + sizeBefore);
+        logger.info("Кол-во контактов ДО удаления: " + sizeBefore);
+        // Удалить контакт `CONTACT_NAME`
+        app.getContactHelper().deleteOneContact(app.getContactHelper().CONTACT_NAME);
+        //pause(1000);
+        app.wait.until(driver -> app.getContactHelper().getContactsCount() < sizeBefore);
+        // Количество контактов ПОСЛЕ удаления.
+        int sizeAfter = app.getContactHelper().getContactsCount();
+        System.out.println("Кол-во контактов ПОСЛЕ удаления: " + sizeAfter);
+        // Сравнить переменные до и после удаления
+        Assert.assertEquals(sizeAfter, sizeBefore - 1);
+    }
+
+    @Test
+    public void deleteAllContactsTest(){
+        while(hasContacts()){
+            int sizeBefore = app.getContactHelper().getContactsCount();
+            System.out.println("Кол-во контактов ДО удаления: " + sizeBefore);
+            //click(By.className(CONTACT_LOCATOR));
+            app.getContactHelper().click(app.getContactHelper().LOCATOR);
+            app.getHomePageHelper().click(app.getContactHelper().BUTTON_REMOVE);
+            app.wait.until(driver -> app.getContactHelper().getContactsCount() < sizeBefore);
+        }
+        if(!hasContacts()){
+            System.out.println("Все контакты удалены: " + app.getContactHelper().getContactsCount());
+        }
+        Assert.assertEquals(app.getContactHelper().getContactsCount(),0,"Не все контакты были удалены: " + app.getContactHelper().getContactsCount());
+    }
+
+    private boolean hasContacts() {
+        return app.getContactHelper().isElementPresent(By.className(app.getContactHelper().CONTACT_LOCATOR));
+    }
+}
